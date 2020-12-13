f303cdcc4dd5f4ac87860f7639175d2256a51b73
Павел <Pavel@mail.ru>
2020-12-13 13:00:08 +0300

    add

--- Assets/charController.cs
+++ Assets/charController.cs
@@ -12,6 +12,8 @@ public class charController: MonoBehaviour
     public Slider sliderCheerfulness;
     public Slider sliderKnowledge;
 
+    public GameObject textCmert;
+    public bool healthChar = true; // жив ли игрок
     public  int maxAll  = 100; // максимальное значения всех показателей
     public  int health;// Здоровье игрока
     public  int psyche;// Психика игрока
@@ -38,13 +40,25 @@ public class charController: MonoBehaviour
         sliderHealth.value = health;
         camera.transform.position = new Vector3(transform.position.x , 35, transform.position.z); // Передвижение камеры за игроком
     }
-    public void cheerfulnessMinusTime(){
+    void Update(){
+        
+    }
+    public void cheerfulnessMinusTime(){ // Обнимание бодрости с временем
         if(cheerfulness > 0){
             cheerfulness = cheerfulness - 2;
             SetSpeed();
         }
     }
-    public void SetSpeed(){
+    public void healthMinus(int yron){ // Получение урона
+        health = health - yron;
+        CheckHealth();
+    }
+    public void CheckHealth(){ // Проверка здоровья
+        if(health <= 0){
+            Instantiate(textCmert);
+        }
+    }
+    public void SetSpeed(){ // Изменения скорости взависимости от бодрости
         if(cheerfulness > 80){
             speed = 5;
         }else if(cheerfulness > 60){
